c39f9fc3c80cf5b05af862c6bbc429ef02bf5a75
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2025-08-11 23:21:07 +0200

    implement  and  filters for serialization feature

--- src/Traits/UseJsonSerializable.php
+++ src/Traits/UseJsonSerializable.php
@@ -6,6 +6,9 @@ namespace Rkt\MageData\Traits;
 
 trait UseJsonSerializable
 {
+    private ?array $onlyKeys = null;
+    private ?array $exceptKeys = null;
+
     public function toArray(): array
     {
         return $this->jsonSerialize();
@@ -18,11 +21,38 @@ trait UseJsonSerializable
 
     public function jsonSerialize(): array
     {
-        return get_object_vars($this);
+        $data = get_object_vars($this);
+
+        if ($this->onlyKeys !== null) {
+            return array_intersect_key($data, array_flip($this->onlyKeys));
+        }
+
+        if ($this->exceptKeys !== null) {
+            return array_diff_key($data, array_flip($this->exceptKeys));
+        }
+
+        return $data;
     }
 
     public function __toString(): string
     {
         return $this->toJson();
     }
+
+    public function only(string ...$keys): static
+    {
+        $clone = clone $this;
+        $clone->onlyKeys = $keys;
+        $clone->exceptKeys = null;
+        return $clone;
+    }
+
+    public function except(string ...$keys): static
+    {
+        $clone = clone $this;
+        $clone->exceptKeys = $keys;
+        $clone->onlyKeys = null;
+        return $clone;
+    }
+
 }
